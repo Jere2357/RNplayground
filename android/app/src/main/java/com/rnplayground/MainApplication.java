6fa1fd505dc7faa3ceb7871d4357b14c3b72f889
Jere2357 <jeremiah@pearlpay.io>
2020-01-06 15:57:13 +0800

    change indention that is compatible with intellij

--- android/app/src/main/java/com/rnplayground/MainApplication.java
+++ android/app/src/main/java/com/rnplayground/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactlibrary.RNReactNativeDocViewerPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -22,7 +23,7 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
+          List<ReactPackage> packages = new PackageList(this).getPackages(); new RNReactNativeDocViewerPackage();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
           return packages;
--- src/containers/BiometricsPage.js
+++ src/containers/BiometricsPage.js
@@ -2,136 +2,148 @@ import React, {Component} from 'react';
 import ReactNativeBiometrics from 'react-native-biometrics';
 import {View, Button, StyleSheet} from 'react-native';
 
-class BiometricsPage extends Component{
+class BiometricsPage extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props){
-        super(props);
-    }
+  async isSensorAvailable() {
+    //isSensorAvailable()
 
-    async isSensorAvailable(){
-        //isSensorAvailable() 
+    ReactNativeBiometrics.isSensorAvailable().then(resultObject => {
+      const {available, biometryType} = resultObject;
 
-        ReactNativeBiometrics.isSensorAvailable()
-        .then((resultObject) => {
-            const { available, biometryType } = resultObject
-        
-            if (available && biometryType === ReactNativeBiometrics.TouchID) {
-            alert('TouchID is supported')
-            } else if (available && biometryType === ReactNativeBiometrics.FaceID) {
-            alert('FaceID is supported')
-            } else if (available && biometryType === ReactNativeBiometrics.Biometrics) {
-            alert('Biometrics is supported')
-            } else {
-            alert('Biometrics not supported')
-            }
-        })
-    }
+      if (available && biometryType === ReactNativeBiometrics.TouchID) {
+        alert('TouchID is supported');
+      } else if (available && biometryType === ReactNativeBiometrics.FaceID) {
+        alert('FaceID is supported');
+      } else if (
+        available &&
+        biometryType === ReactNativeBiometrics.Biometrics
+      ) {
+        alert('Biometrics is supported');
+      } else {
+        alert('Biometrics not supported');
+      }
+    });
+  }
 
-    async createKeys() {
-        // createKeys()
+  async createKeys() {
+    // createKeys()
 
-        ReactNativeBiometrics.createKeys('Confirm fingerprint')
-            .then((resultObject) => {
-                const { publicKey } = resultObject
-                alert(publicKey)
-                // sendPublicKeyToServer(publicKey)
-            })
-    }
+    ReactNativeBiometrics.createKeys('Confirm fingerprint')
+      .then(resultObject => {
+        const {publicKey} = resultObject;
+        alert(publicKey);
+        // sendPublicKeyToServer(publicKey)
+      })
+      .catch(() => alert('biometrics not supported'));
+  }
 
-    async biometricKeysExists(){
-        //biometricKeysExist()
+  async biometricKeysExists() {
+    //biometricKeysExist()
 
-        ReactNativeBiometrics.biometricKeysExist()
-            .then((resultObject) => {
-                const { keysExist } = resultObject
-            
-                if (keysExist) {
-                alert(keysExist+' Keys exist')
-                } else {
-                alert(keysExist+' Keys do not exist or were deleted')
-                }
-            })
-    }
+    ReactNativeBiometrics.biometricKeysExist().then(resultObject => {
+      const {keysExist} = resultObject;
 
-    async deleteKeys(){
-        // //deleteKeys()
+      if (keysExist) {
+        alert(keysExist + ' Keys exist');
+      } else {
+        alert(keysExist + ' Keys do not exist or were deleted');
+      }
+    });
+  }
 
-        ReactNativeBiometrics.deleteKeys()
-            .then((resultObject) => {
-                const { keysDeleted } = resultObject
-            
-                if (keysDeleted) {
-                alert(keysDeleted+' Successful deletion')
-                } else {
-                alert(keysDeleted+' Unsuccessful deletion because there were no keys to delete')
-                }
-            })
-    }
+  async deleteKeys() {
+    // //deleteKeys()
 
-    async createSignature(){
-        let epochTimeSeconds = Math.round((new Date()).getTime() / 1000).toString()
-        let payload = epochTimeSeconds + 'Current Date for Creating finger print'
+    ReactNativeBiometrics.deleteKeys().then(resultObject => {
+      const {keysDeleted} = resultObject;
 
-        ReactNativeBiometrics.createSignature({
-                promptMessage: 'Sign in',
-                payload: payload
-            })
-            .then((resultObject) => {
-                const { success, signature } = resultObject
-            
-                if (success) {
-                alert(signature)
-                // verifySignatureWithServer(signature, payload)
-                }
-            })
-            .catch(() => alert('Failed creating signature key'))
-    
-    }
+      if (keysDeleted) {
+        alert(keysDeleted + ' Successful deletion');
+      } else {
+        alert(
+          keysDeleted +
+            ' Unsuccessful deletion because there were no keys to delete',
+        );
+      }
+    });
+  }
 
-    async simplePrompt(){
-        //simplePrompt()
+  async createSignature() {
+    let epochTimeSeconds = Math.round(new Date().getTime() / 1000).toString();
+    let payload = epochTimeSeconds + 'Current Date for Creating finger print';
 
-        ReactNativeBiometrics.simplePrompt({
-            promptMessage: 'Checking if biometric is provided in this device'
-        })
-            .then((resultObject) => {
-                const { success } = resultObject
-            
-                if (success) {
-                alert('successful biometrics provided')
-                } else {
-                alert('user cancelled biometric prompt')
-                }
-            })
-            .catch(() => {
-                alert('biometrics prompt failed')
-            })
-    }
+    ReactNativeBiometrics.createSignature({
+      promptMessage: 'Sign in',
+      payload: payload,
+    })
+      .then(resultObject => {
+        const {success, signature} = resultObject;
 
-    render(){
-        return(
-            <View style={styles.container}>
-                <Button title={'What sensor is available'} onPress={() => this.isSensorAvailable()} />
-                <Button title={'create public key'}  onPress={() => this.createKeys()} />
-                <Button title={'public key exist ?'} onPress={() => this.biometricKeysExists()} />
-                <Button title={'delete public key'} onPress={() => this.deleteKeys()} />
-                <Button title={'create signature key'} onPress={() => this.createSignature()} />
-                <Button title={'simple prompt of biometrics'} onPress={() => this.simplePrompt()} />
-            </View>
-        ); 
-    }
+        if (success) {
+          alert(signature);
+          // verifySignatureWithServer(signature, payload)
+        }
+      })
+      .catch(() => alert('Failed creating signature key'));
+  }
 
+  async simplePrompt() {
+    //simplePrompt()
+
+    ReactNativeBiometrics.simplePrompt({
+      promptMessage: 'Checking if biometric is provided in this device',
+    })
+      .then(resultObject => {
+        const {success} = resultObject;
+
+        if (success) {
+          alert('successful biometrics provided');
+        } else {
+          alert('user cancelled biometric prompt');
+        }
+      })
+      .catch(() => {
+        alert('biometrics prompt failed');
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button
+          title={'What sensor is available'}
+          onPress={() => this.isSensorAvailable()}
+        />
+        <Button title={'create public key'} onPress={() => this.createKeys()} />
+        <Button
+          title={'public key exist ?'}
+          onPress={() => this.biometricKeysExists()}
+        />
+        <Button title={'delete public key'} onPress={() => this.deleteKeys()} />
+        <Button
+          title={'create signature key'}
+          onPress={() => this.createSignature()}
+        />
+        <Button
+          title={'simple prompt of biometrics'}
+          onPress={() => this.simplePrompt()}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flexDirection: 'column',
-      display: 'flex',
-      justifyContent: 'center',
-      backgroundColor: 'rgba(127, 237, 245, 1)',
-      height: 800,
-    },
-  });
-  
+  container: {
+    flexDirection: 'column',
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(127, 237, 245, 1)',
+    height: 800,
+  },
+});
 
 export default BiometricsPage;
